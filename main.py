8754fa95ddba24cdef8c69abaa4a7bd5484c15a6
Max Baranov <146020380+maxbaranov16@users.noreply.github.com>
2023-11-18 14:13:07 +0300

    Добавлена возможность записи в файл найденных слов
    Теперь после нахождения в строке удовлетворяющих нас подстрок происходит запись их в файл. Записываются найденные слова как из первого примера, заданного в программе, так и из второго примера, который пользователь задаёт сам.

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ def search_word(alphabet, s):
 alphabet1 = "abc"
 s1 = "abccbcbabaabcabbbcccaaacbcbcbcbacbacbabcabcbabaabcabbbbba"
 result1 = search_word(alphabet1,s1)
+with open("result1.txt", "w") as file:
+    file.write(str(result1))
 print(result1)
 
 alphabet_input = input("Enter the alphabet: ")
 string_input = input("Enter the string: ")
 result2 = search_word(alphabet_input, string_input)
+with open("result2.txt", "w") as file:
+    file.write(str(result2))
 print(result2)
